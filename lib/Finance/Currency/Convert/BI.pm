67a503dffc6d1ae442702b3382420ef016dc91cf
perlancar (on pc-office) <perlancar@gmail.com>
2017-07-10 20:23:49 +0700

    Replace Log::Any with Log::ger

--- lib/Finance/Currency/Convert/BI.pm
+++ lib/Finance/Currency/Convert/BI.pm
@@ -6,7 +6,7 @@ package Finance::Currency::Convert::BI;
 use 5.010001;
 use strict;
 use warnings;
-use Log::Any::IfLOG '$log';
+use Log::ger;
 
 use DateTime::Format::Indonesian;
 use Parse::Number::ID qw(parse_number_id);

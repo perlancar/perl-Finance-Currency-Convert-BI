cdec3f7a2776a3762af56043d7af9012784c54a1
perlancar (on netbook-zenbook-ux305) <perlancar@gmail.com>
2018-06-17 14:09:33 +0700

    Add function: get_currencies()

--- lib/Finance/Currency/Convert/BI.pm
+++ lib/Finance/Currency/Convert/BI.pm
@@ -45,7 +45,7 @@ sub get_jisdor_rates {
     } else {
         require Mojo::UserAgent;
         my $ua = Mojo::UserAgent->new;
-        my $tx = $ua->get("http://www.bi.go.id/id/moneter/informasi-kurs/referensi-jisdor/Default.aspx",
+        my $tx = $ua->get("https://www.bi.go.id/id/moneter/informasi-kurs/referensi-jisdor/Default.aspx",
                       {'User-Agent' => 'Mozilla/4.0'});
         my $res = $tx->success;
         if ($res) {
@@ -72,19 +72,105 @@ sub get_jisdor_rates {
     [200, "OK", \@res];
 }
 
+$SPEC{get_currencies} = {
+    v => 1.1,
+    summary => "Extract currency data from Bank Indonesia's page",
+    result => {
+        description => <<'_',
+
+Will return a hash containing key `currencies`.
+
+The currencies is a hash with currency symbols as keys and prices as values.
+
+Tha values is a hash with these keys: `buy` and `sell`.
+
+_
+    },
+};
+sub get_currencies {
+    #require Mojo::DOM;
+    require Parse::Date::Month::ID;
+    require Parse::Number::EN;
+    #require Parse::Number::ID;
+    require Time::Local;
+
+    my %args = @_;
+
+    #return [543, "Test parse failure response"];
+
+    my $url = "https://www.bi.go.id/id/moneter/informasi-kurs/transaksi-bi/Default.aspx";
+
+    my $page;
+    if ($args{_page_content}) {
+        $page = $args{_page_content};
+    } else {
+        require Mojo::UserAgent;
+        my $ua = Mojo::UserAgent->new;
+        $ua->transactor->name("Mozilla/4.0");
+        my $tx = $ua->get($url);
+        unless ($tx->success) {
+            my $err = $tx->error;
+            return [500, "Can't retrieve BCA page ($url): ".
+                        "$err->{code} - $err->{message}"];
+        }
+        $page = $tx->res->body;
+    }
+    log_trace "page: [[$page]]";
+
+    my %currencies;
+    my @recs;
+    while ($page =~ m!<td>(\w{3})  </td><td class="alignRight">(\d(?:\.\d+)?)</td><td style="text-align:right;">($Parse::Number::EN::Pat)</td><td style="text-align:right;">($Parse::Number::EN::Pat)</td>!g) {
+        push @recs, [$1, $2, $3, $4];
+    }
+
+    for (@recs) {
+        my $mult = Parse::Number::EN::parse_number_en(text => $_->[1])
+            or return [543, "Can't parse number '$_->[1]'"];
+        my $sell = Parse::Number::EN::parse_number_en(text => $_->[2])
+            or return [543, "Can't parse number '$_->[2]'"];
+        my $buy  = Parse::Number::EN::parse_number_en(text => $_->[3])
+            or return [543, "Can't parse number '$_->[3]'"];
+        $currencies{$_->[0]} = {
+            sell => $sell / $mult,
+            buy =>  $buy  / $mult,
+        };
+    }
+
+    if (keys %currencies < 3) {
+        return [543, "Check: no/too few currencies found"];
+    }
+
+    my $mtime;
+  GET_MTIME:
+    {
+        unless ($page =~ m!Update Terakhir.+>((\d+) (\w+) (\d{4}))<!) {
+            log_warn "Cannot extract last update time";
+            last;
+        }
+        my $mon = Parse::Date::Month::ID::parse_date_month_id(text=>$3) or do {
+            log_warn "Cannot recognize month name '$3' in last update time '$1'";
+            last;
+        };
+        $mtime = Time::Local::timegm(0, 0, 0, $2, $mon-1, $4) - 7*3600;
+    }
+
+    [200, "OK", {
+        mtime => $mtime,
+        currencies => \%currencies,
+    }];
+}
+
 1;
 # ABSTRACT:
 
 =head1 SYNOPSIS
 
- use Finance::Currency::Convert::BI qw(get_jisdor_rates);
+ use Finance::Currency::Convert::BI qw(get_currencies get_jisdor_rates);
 
  my $res = get_jisdor_rates();
 
 
 =head1 DESCRIPTION
-
-B<EARLY RELEASE>.
 
 
 =head1 SEE ALSO
